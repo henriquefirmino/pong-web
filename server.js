8bd303230db57424f7f9aa5f0310706b20fc9ac5
henriquefirmino <henriquecasarini4@gmail.com>
2021-10-22 14:52:30 -0300

    serverjs

---
+++ server.js
@@ -0,0 +1,17 @@
+var connections = [];
+var express = require('express');
+var app = express();
+var server = app.listen(3000);
+app.use(express.static('public'));
+
+console.log("RUNNIG");
+
+var socket = require('socket.io');
+var io = socket(server);
+
+io.sockets.on('connection', function(socket) {
+    connections.push(socket);
+    socket.on('start', function(data) {
+        console.log("Usuarios conectados: " + connections.length);
+    });
+});
