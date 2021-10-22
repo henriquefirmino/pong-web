e4f3d2ad0dd86786cc6fa69091c8d95c55a4a16d
henriquefirmino <henriquecasarini4@gmail.com>
2021-10-22 15:19:41 -0300

    ball

---
+++ public/player.js
@@ -0,0 +1,24 @@
+function Player() {
+    this.x = 0;
+    this.y = height/2;
+    this.velocity = 4;
+    this.w = 20;
+    this.h = 80;
+    this.points = 0;
+
+    this.show = function() {
+        rectMode(CENTER);
+        rect(this.x, this.y, this.w, this.h)
+    }
+
+    this.move = function(b) {
+        //if(b.x < width/2) {
+            if(p.y < mouseY) {
+                p.y += p.velocity;
+            }
+            else if (p.y > mouseY) {
+                p.y -= p.velocity;
+            }
+        }
+    //}
+}
